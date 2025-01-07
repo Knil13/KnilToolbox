948697916514b32566dbc423fd56bef66dd211c8
Yohann <yohann.maillard@frequentiel.com>
2025-01-07 16:08:57 +0100

    ajout commande /pokelist et /pokegive + event capture

--- src/main/java/fr/knil/kniltoolbox/Kniltoolbox.java
+++ src/main/java/fr/knil/kniltoolbox/Kniltoolbox.java
@@ -3,9 +3,16 @@ package fr.knil.kniltoolbox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.cobblemon.mod.common.api.Priority;
+import com.cobblemon.mod.common.api.events.CobblemonEvents;
+import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent;
+import com.cobblemon.mod.common.pokemon.Pokemon;
 
 import fr.knil.kniltoolbox.util.ModCommands;
+import kotlin.Unit;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 
 public class Kniltoolbox implements ModInitializer {
@@ -18,7 +25,28 @@ public class Kniltoolbox implements ModInitializer {
     	LOGGER.info("Lancement de Knil ToolBox");
 		ModCommands.registerCommands();	
 		
-		
+		//abonnement à l'event de capture de poké
+		CobblemonEvents.POKEMON_CAPTURED.subscribe(Priority.NORMAL, this::onCaptured);			
         
-    }    
+    }      
+    
+    private Unit onCaptured(PokemonCapturedEvent pokemonCapturedEvent) {
+    	//variable stockant le joueur ayant capturer un poké
+    	ServerPlayerEntity player = pokemonCapturedEvent.getPlayer();
+    	
+    	//Variable contenant le pokemon capturé
+    	Pokemon capturedPoke = pokemonCapturedEvent.getPokemon();
+    	
+    	//generation d'un nombre aleatoire entre 0 et 100;
+    	int randomInt = (int) (Math.random() * 101);
+    	//Si le nombre est superieur à 50, le pokemon devient shiny.
+    	if(randomInt>50) {
+    		//changement du pokémon en shiny
+    		capturedPoke.setShiny(true);
+    		player.sendMessage(Text.of("Bravo ! ton pokemon est devenu shiny !"), false);
+    	}
+
+        return Unit.INSTANCE;
+
+    }
 }
--- src/main/java/fr/knil/kniltoolbox/util/ModCommands.java
+++ src/main/java/fr/knil/kniltoolbox/util/ModCommands.java
@@ -1,12 +1,14 @@
 package fr.knil.kniltoolbox.util;
 
-
+import com.cobblemon.mod.common.Cobblemon;
+import com.cobblemon.mod.common.api.pokeball.PokeBalls;
 import com.cobblemon.mod.common.api.pokemon.Natures;
 import com.cobblemon.mod.common.api.pokemon.PokemonSpecies;
 import com.cobblemon.mod.common.api.pokemon.stats.Stats;
 import com.cobblemon.mod.common.api.storage.party.PlayerPartyStore;
+import com.cobblemon.mod.common.api.types.tera.TeraTypes;
+import com.cobblemon.mod.common.pokemon.Gender;
 import com.cobblemon.mod.common.pokemon.Pokemon;
-import com.cobblemon.mod.common.pokemon.Species;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
@@ -18,6 +20,8 @@ import net.minecraft.text.Text;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
+import java.util.UUID;
+
 
 public class ModCommands {
 
@@ -27,82 +31,78 @@ public class ModCommands {
 		 CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {            
 	        	
 			// commande /givepoke
-	            dispatcher.register(literal("givepoke").executes(ModCommands::givepoke));
+	            dispatcher.register(literal("pokegive").executes(ModCommands::pokegive));
 	            
 	         // commande /test
-	            dispatcher.register(literal("testpoke").executes(ModCommands::test));
+	            dispatcher.register(literal("pokelist").executes(ModCommands::pokelist));
 		 });
 	}
 		 
-	private static int test(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-				
-		ServerPlayerEntity player = context.getSource().getPlayer();		
-		
-		
-		PlayerPartyStore PPS = new PlayerPartyStore(player.getUuid());
+	
+	private static int pokelist(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		//initialisation des variables
+		ServerPlayerEntity player = context.getSource().getPlayer();	//recuperation du joueur
+		PlayerPartyStore playerParty = Cobblemon.INSTANCE.getStorage().getParty(player); //recuperation de l'equipe du joueur	
 		Pokemon poke;
 		String pokeName;
+		Boolean shiny;
+		String isShiny = "";			
 		
-		player.sendMessage(Text.literal("test1"), false);
-		poke = PPS.get(1);
-		
-		player.sendMessage(Text.literal("test2"), false);
-		
-		int pokelevel = poke.getLevel();
-		
-		player.sendMessage(Text.literal("test3"), false);
-		
-		/*pokeName = poke.getSpecies().getName();
-		player.sendMessage(Text.literal(pokeName), false);*/
-		
-		/*for(int i = 0; i < 5; i++) {
-			
-			player.sendMessage(Text.literal("pokemon " + i + " : "), false);
-			poke = PPS.get(i);	
-			pokeName = poke.getSpecies().getName();
-			player.sendMessage(Text.literal(pokeName), false);
-		}*/
-		
+		//boucle for parcourant la liste des pokémon du joueur, slot par slot
+		for(int i = 0; i <= 5; i++) {			
+			poke = playerParty.get(i);	//recuperation du pokemon du slot i
+			if (poke != null) {
+				pokeName = poke.getSpecies().getName(); // recuperation du nom de son espece.
+				shiny = poke.getShiny();	// recuperation du fait qu'il soit shiny ou non
+				if (shiny) isShiny = " §e(shiny)";	// s'il est shiny, alors le rajouter dans la chaine
+				else isShiny = "";					
+				player.sendMessage(Text.literal("§bSlot " + (i+1) + " : " + pokeName + isShiny), false);	//afficher au joueur le pokemon du slot (avec une indication s'il est shiny ou non)
+			}
+			else player.sendMessage(Text.literal("§3Slot " + (i+1) + " : vide"), false); // afficher que le slot est vide
+		}
 		
 		return 1;
 	}
 	
 	
-	private static int givepoke(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-		ServerPlayerEntity player = context.getSource().getPlayer();	
-				
-		Pokemon poke = new Pokemon();
-		Species espece = PokemonSpecies.INSTANCE.getByName("Charmander");		
+	private static int pokegive(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		//Recuperation du joueur dans une var
+		ServerPlayerEntity player = context.getSource().getPlayer();
 		
-		poke.setSpecies(espece);;		
-		
-		
-		poke.setLevel(5);
-		
+		//recuperation de la team du joueur
+		PlayerPartyStore playerParty = Cobblemon.INSTANCE.getStorage().getParty(player);	
+			
+		//creation du pokémon à give
+		Pokemon poke = new Pokemon();		
+		poke.setCaughtBall(PokeBalls.INSTANCE.getLOVE_BALL());	//configuration de la ball
+		poke.setSpecies(PokemonSpecies.INSTANCE.getByName("pikachu"));	//configuration de l'espece		
+		poke.setLevel(5); //configuration du niveau
+		poke.setUuid(UUID.randomUUID()); //configuration de l'UUID		
+		//configuration des IVs
 		poke.setIV(Stats.ATTACK, 31);
 		poke.setIV(Stats.DEFENCE, 31);
 		poke.setIV(Stats.HP, 31);
 		poke.setIV(Stats.SPECIAL_ATTACK, 31);
 		poke.setIV(Stats.SPECIAL_DEFENCE, 31);
 		poke.setIV(Stats.SPEED, 31);
-		
+		//configuration des EV
 		poke.setEV(Stats.ATTACK, 0);
 		poke.setEV(Stats.DEFENCE, 0);
 		poke.setEV(Stats.HP, 0);
 		poke.setEV(Stats.SPECIAL_ATTACK, 0);
 		poke.setEV(Stats.SPECIAL_DEFENCE, 0);
-		poke.setEV(Stats.SPEED, 0);
+		poke.setEV(Stats.SPEED, 0);	
 		
-		poke.setShiny(true);
-		poke.setNature(Natures.INSTANCE.getTIMID());
+		poke.setShiny(true);	//configuration du caractere shiny
+		poke.setNature(Natures.INSTANCE.getTIMID());	//configuration de la nature	
+		poke.setNickname(Text.literal("GiftMon"));		//configuration du surnom
+		poke.setGender(Gender.MALE);	//configuration du genre
+		poke.setOriginalTrainer(player.getUuid());	//configuration du dresseur d'origine
+		poke.setTeraType(TeraTypes.getDRAGON());	//configuration du teraType
 		
-		poke.setNickname(Text.literal("GiftMon"));
-		;
-		
-		PlayerPartyStore PPS = new PlayerPartyStore(player.getUuid());
-		PPS.add(poke);		
-		
-		player.sendMessage(Text.literal("Tu as un cadeau :)"), false);
+		//ajout du pokemon à la team : 
+		playerParty.add(poke); //Attention, je ne verifie pas encore si la team est pleine ou non.
+		player.sendMessage(Text.literal("§aCadeau : un " + poke.getSpecies().getName() + " §eshiny !!"), false);		
 
 		return 1;
 	}	
